27b042f8aa71b246fbea8e88152da3ab8ecbdc2e
Marek Macháček <machacek1marek@gmail.com>
2026-04-20 15:16:46 +0200

    pridaní .py souboru

---
+++ miny.py
@@ -0,0 +1,16 @@
+print('chodíš do prace?')
+print('pokud ano napiš 0, pokud ne napiš 1')
+odpoved=int(input('tvoje odpověd:'))
+
+#rozthodovač
+if odpoved==0:
+        skodpoved ='ano'
+elif  odpoved==1:
+        skodpoved ='ne'
+else:
+    skodpoved ='není definovaná skontrolejte vstupní udaje'
+print('tvoje odpoved je:',skodpoved)
+
+        
+
+    
