eef6adcfc0bb5b9870d08868523eb3b437b2ae72
Marek Macháček <machacek1marek@gmail.com>
2026-05-25 16:59:45 +0200

    jde jit i ze hry do nastravení funkční

--- miny.py
+++ miny.py
@@ -17,18 +17,16 @@ vlajky = []             # Kde jsou vlajky?
 hra_bezi = True         # Běží hra?
 cas_startu = 0          # Kdy se spustila
 prvni_klik = True       # Garantovaně bez miny
-print("definovano")
 
 ##definice command
-def back():
-    print("jdu do lobby")
-    herni_okno.withdraw()
-    lobby_okno.deiconify()
-
-def settings():
-    print("settings okno")
-    lobby_okno.withdraw()
-    settings_okno.deiconify()
+def prepni_na(cilove_okno):
+    # Projdeme všechna okna v seznamu a skryjeme je
+    for okno in vsechna_okna:
+        okno.withdraw()
+    
+    # Ukážeme jen to jedno, které chceme
+    cilove_okno.deiconify()
+    print(f"Přepnuto na: {cilove_okno.title()}")
 
 def settings_back():
     global SIRKA, VYSKA, POCET_MIN
@@ -42,14 +40,18 @@ def settings_back():
     else:
         SIRKA = int(hodnota_x)
         VYSKA = SIRKA
+    if hodnota_m == "":
+        POCET_MIN = SIRKA
+        print("mina auto")
+    else:
         POCET_MIN = int(hodnota_m)
+        print("mina nastaven")
     # 4. výpis
     print(f"SIRKA: {SIRKA}")
     print(f"VYSKA: {VYSKA}")
     print(f"p_min: {POCET_MIN}")
     print("zpet do lobby")
-    settings_okno.withdraw()
-    lobby_okno.deiconify()
+    prepni_na(lobby_okno)
 
 def play():
     global SIRKA, VYSKA, POCET_MIN
@@ -59,9 +61,9 @@ def play():
         print("sirka chybí, nastavuji 10")
     if POCET_MIN == "":
         POCET_MIN = 10
-        print("pocet m chybí, nastavuji 10")   
-    lobby_okno.withdraw()
-    herni_okno.deiconify()
+        print("pocet m chybí, nastavuji 10")
+    
+    prepni_na(herni_okno)
     herni_okno.geometry(str(31* SIRKA) + "x" + str(26* VYSKA+33))
 
     for r in range(VYSKA):
@@ -84,7 +86,7 @@ def play():
 ###grafika  
 ## Vytvoření lobby okna
 lobby_okno = tk.Tk()
-lobby_okno.title("Miny_lobby")
+lobby_okno.title("lobby_okno")
 lobby_okno.geometry("600x500")
 #logo lable
 logo_label = tk.Label(lobby_okno, text="Miny", font=("Arial", 25))
@@ -96,12 +98,12 @@ logo.grid(pady=(5, 10))
 play_tlacitko = tk.Button(lobby_okno, text="Play 🔺", font=("Arial", 25), bg="green", command=play)
 play_tlacitko.grid(pady=(10, 25))
 #settings buton
-settings_tlacitko = tk.Button(lobby_okno, text="Settings", font=("Arial", 10), bg="grey", command=settings)
+settings_tlacitko = tk.Button(lobby_okno, text="⚙️", font=("Arial", 10), bg="grey", command=lambda: prepni_na(settings_okno))
 settings_tlacitko.grid(pady=(150, 25))
 
 ##vytvorení settings okna
 settings_okno = tk.Toplevel(lobby_okno)
-settings_okno.title("settings")
+settings_okno.title("settings_okno")
 settings_okno.geometry("300x300")
 #settings bvak buton
 back_tlacitko = tk.Button(settings_okno, text="⬅️", font=("Arial", 10), bg="grey", command=settings_back)
@@ -122,12 +124,19 @@ settings_okno.withdraw()
 
 ## Vytvoření herní okna
 herni_okno = tk.Toplevel(lobby_okno)
-herni_okno.title("Miny_hra")
+herni_okno.title("herni_okno")
 herni_okno.geometry("200x200")
 #back buton
-back_tlacitko = tk.Button(herni_okno, text="⬅️", font=("Arial", 10), bg="grey", command=back)
+back_tlacitko = tk.Button(herni_okno, text="⬅️", font=("Arial", 10), bg="grey", command=lambda: prepni_na(lobby_okno))
 back_tlacitko.grid(pady=(0, 5))
+#settings buton
+settings_tlacitko = tk.Button(herni_okno, text="⚙️", font=("Arial", 10), bg="grey", command=lambda: prepni_na(settings_okno))
+settings_tlacitko.grid(row= 0, column=1, pady=(0, 5))
+#ukoncení
 herni_okno.withdraw()
+
+# Seznam všech oken pro snadnou hromadnou práci
+vsechna_okna = [lobby_okno, settings_okno, herni_okno]
 
 # Spuštění programu
 lobby_okno.mainloop()
